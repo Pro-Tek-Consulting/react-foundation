3f840e482692d25ceb4803d21412731408ce9620
nehaswaroop <neha.thakur.swaroop@gmail.com>
2023-02-23 02:12:07 -0800

    Class based component and State

--- neha-app/src/App.js
+++ neha-app/src/App.js
@@ -1,4 +1,8 @@
 import "./App.css";
+import HelloMsg from "./components/HelloMsg";
+import PersonalTable from "./components/PersonalTable";
+import Link from "./components/Link";
+import Counter from "./components/Counter";
 
 function App() {
 	const user = {
@@ -10,26 +14,46 @@ function App() {
 
 	const element = (
 		<div>
-			<h3>
-				Hello, {user.firstName} {user.lastName}!
-			</h3>
-			<table>
-				<th>Name</th>
-				<th>Gender</th>
-				<th>Age</th>
-				<tr>
-					<td>
-						{user.firstName} {user.lastName}
-					</td>
-					<td>{user.gender}</td>
-					<td>{user.age}</td>
-				</tr>
-			</table>
-			<a href="https://www.reactjs.org">Click Me!</a>
+			<HelloMsg user={user}></HelloMsg>
+			<PersonalTable user={user}></PersonalTable>
+			<Link url="google.com">Google</Link>
+			<Counter startAt={4} />
 		</div>
 	);
-
 	return element;
 }
-
 export default App;
+
+// function App() {
+// 	const user = {
+// 		firstName: "Neha",
+// 		lastName: "Thakur",
+// 		gender: "Female",
+// 		age: 29,
+// 	};
+
+// 	const element = (
+// 		<div>
+// 			<h3>
+// 				Hello, {user.firstName} {user.lastName}!
+// 			</h3>
+// 			<table>
+// 				<th>Name</th>
+// 				<th>Gender</th>
+// 				<th>Age</th>
+// 				<tr>
+// 					<td>
+// 						{user.firstName} {user.lastName}
+// 					</td>
+// 					<td>{user.gender}</td>
+// 					<td>{user.age}</td>
+// 				</tr>
+// 			</table>
+// 			<a href="https://www.reactjs.org">Click Me!</a>
+// 		</div>
+// 	);
+
+// 	return element;
+// }
+
+// export default App;
---
+++ neha-app/src/components/Counter.jsx
@@ -0,0 +1,30 @@
+import React, { Component } from "react";
+
+class Counter extends Component {
+	constructor(props) {
+		super(props);
+		this.state = { currentCount: this.props.startAt };
+		this.addHandleClick = this.addHandleClick.bind(this);
+		this.decreaseHandleClick = this.decreaseHandleClick.bind(this);
+	}
+	addHandleClick(e) {
+		this.setState({ currentCount: this.state.currentCount + 1, name: this.state.currentCount });
+	}
+	decreaseHandleClick(e) {
+		this.setState({ currentCount: this.state.currentCount - 1, name: this.state.currentCount });
+	}
+
+	render() {
+		return (
+			<div>
+				<span> This is a counter.</span>
+				<h1> Current Count: {this.state.currentCount}</h1>
+				<button onClick={this.addHandleClick}>Click Me to Increase</button>
+				<button onClick={this.decreaseHandleClick}>Click Me to Decrease</button>
+				{this.state.name && <h4>Last updated by {this.state.name}</h4>}
+			</div>
+		);
+	}
+}
+
+export default Counter;
