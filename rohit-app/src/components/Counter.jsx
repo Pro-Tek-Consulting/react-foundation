830ee22027eee3eb6bc24872084d38d2ee3633f9
Rohit Maddula <rohitteja.maddula@microfocus.com>
2023-02-24 18:33:41 -0500

    Added lifecyclemethods

--- rohit-app/src/components/Counter.jsx
+++ rohit-app/src/components/Counter.jsx
@@ -8,11 +8,38 @@ class Counter extends Component {
     this.handleClick = this.handleClick.bind(this);
   }
   handleClick(e) {
-    this.setState({ currentCount: this.state.currentCount + 1, name: Math.random() });
+    this.setState({
+      currentCount: this.state.currentCount + 1,
+      name: Math.random(),
+    });
   }
+
+  componentDidMount() {
+    setTimeout(() => this.setState({ name: "Rohit" }), 10000);
+  }
+  shouldComponentUpdate(nextProps, nextState) {
+    // Rendering the component only if
+    // passed props value is changed
+
+    if (nextState.currentCount > 7) {
+      return false;
+    } else {
+      return true;
+    }
+  }
+
+  componentDidUpdate() {
+    alert("Updated");
+  }
+
+  componentWillUnmount() {
+    alert("Component will unmount");
+  }
+
   render() {
+    let counterClasses = "myClasess";
     return (
-      <div>
+      <div className={counterClasses}>
         <span> This is a counter.</span>
         <h1> Current Count: {this.state.currentCount}</h1>
         <button onClick={this.handleClick}>Click Me</button>
